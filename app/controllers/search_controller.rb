ff011f1e708442d93ceae67ba204fa96eafcdda5
Rick Little <rick.little@simplarity.com>
2015-09-11 08:27:53 -0400

    pics by params and audio fixed

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -21,7 +21,7 @@ class SearchController < ApplicationController
 		@audio_url = "http://api.wordnik.com:80/v4/word.json/#{params[:search]}/audio?includeDuplicates=false&useCanonical=false&skip=0&api_key=#{Wordnik.configuration.api_key}"
 		audio_buffer = RestClient.get @audio_url
 		@audio_results = JSON.parse(audio_buffer)
-		@picture_url = "https://openclipart.org/search/json/?query=water"
+		@picture_url = "https://openclipart.org/search/json/?query=#{params[:search]}"
 		pic_buffer = RestClient.get @picture_url
 		pic_results = JSON.parse(pic_buffer)
 		@image= pic_results['payload'][0]['svg']['png_thumb']
